4f85c6e420312629d3799d8c2c9b4e9f57d38042
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-05-03 10:08:40 +0300

    +

--- js/js.js
+++ js/js.js
@@ -82,29 +82,30 @@ let price;
 // }
 
 //коротша версія верхнього прикладу
-switch (stars) {
-  case 1:
-    price = 20;
-    break;
+// switch (stars) {
+//   case 1:
+//     price = 20;
+//     break;
 
-  case 2:
-    price = 30;
-    break;
+//   case 2:
+//     price = 30;
+//     break;
 
-  case 3:
-    price = 50;
-    break;
+//   case 3:
+//     price = 50;
+//     break;
 
-  case 4:
-    price = 70;
-    break;
+//   case 4:
+//     price = 70;
+//     break;
 
-  case 5:
-    price = 120;
-    break;
+//   case 5:
+//     price = 120;
+//     break;
 
-  default:
-    console.log("not this numbers");
-}
+//   default:
+//     console.log("not this numbers");
+// }
 
-console.log(price);
+// console.log(price);
+

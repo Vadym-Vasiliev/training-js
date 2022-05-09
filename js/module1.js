d3b55c942b581e9ef5d7d17b07f464532a86c22b
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-05-09 11:38:16 +0300

    +

---
+++ js/module1.js
@@ -0,0 +1,61 @@
+// 9.05  перший день , модуль 1
+
+// додаткові плагіни : CodeSpellChecker , EsLint, template string converter.
+
+// загальна кількість яблук і винограду
+
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+
+// console.log(total);
+
+// заміни вираз перевизначення комбінованим оператором
+
+// let students = 100;
+// // students = students + 50;
+// //або
+// students += 50
+
+// console.log(students)
+
+// розбери приорітет операторів в інструкції присвоєння значення змінної
+
+// const result = 108 + 223 - 2 * 5;
+
+// console.log(result)
+
+// виводить в консоль. заокруглені вгору і вниз значення змінної value
+
+// мето Math.floor(), Math.ceil(), Math.round().
+
+// const value - 27.4;
+
+// console.log(Math.floor(value));
+// console.log(Math.ceil(value));
+// console.log(Math.round(value));
+
+//  склади фразу за допомогою шаблонних рядків
+
+// const companyName = 'Cyberdyne Systems';
+// const repairBots = 150;
+// const defenseBots = 50;
+
+// const message = `${companyName} has ${repairBots + defenseBots} bots in stock`;
+
+// console.log(message);
+
+// розраховує індекс маси тіла.
+
+// let weight = '88,3';
+// let height = '1.75';
+
+// if(weight.includes(',')){
+//   weight = weight.replace(',', '.')
+// }
+// if(height.includes(',')){
+//   weight = weight.replace(',', '.')
+// }
+
+// const bmi = parseFloat(weight) / Math.pow(parseFloat(height), 2);
+// console.log(bmi.toFixed(1));

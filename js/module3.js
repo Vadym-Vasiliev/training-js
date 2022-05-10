f12816c7bbf1558cc572988e27b16e6a69f6ffbf
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-05-10 12:54:37 +0300

    +

--- js/module3.js
+++ js/module3.js
@@ -327,3 +327,68 @@
 
 //============================
 // товари в корзіні
+
+//===========================
+//Модуль 3 частина 2
+
+//spread(распиление)
+//concat-----
+
+// const numbers = [1, 2, 3].concat([4, 5, 6], [7, 8, 9]);
+
+// console.log(numbers);
+
+//spread-------
+
+// const numbers = [...[1, 2, 3], ...[4, 5, 6], ...[7, 8, 9]]; // вставляє елементи  однієї колеції  в другу
+// console.log(numbers);
+
+//пошук маленької або більшої температури
+
+// const temps = [18, 14, 12, 21, 17, 29, 24];
+
+// console.log(Math.max(1, 4, 17, 5, 6));
+// console.log(Math.min(...temps)); //spread
+
+//=================
+//сшиваємо масиви
+
+// const LastWeekTemps = [1, 2, 3];
+// const currentTemps = [4, 5, 6];
+// const nextWeekTemps = [7, 8, 9];
+
+// const allTemps = [...LastWeekTemps, ...currentTemps, ...nextWeekTemps];
+
+// console.log(allTemps);
+
+//роспиляємо обєкти
+
+// const a = { x: 1, y: 2 };
+// const b = { x: 0, z: 3 };
+
+// const c = {
+//   ...a,
+//   ...b,
+// };
+
+// console.log(c);
+
+// приклад
+
+// const defaultSetting = {
+//   theme: "Light",
+//   ShowNotifications: true,
+//   hideSidebar: false,
+// };
+
+// const userSettings = {
+//   ShowNotifications: false,
+//   hideSidebar: true,
+// };
+
+// const finalSettings = {
+//   ...defaultSetting,
+//   ...userSettings,
+// };
+
+// console.log(finalSettings);
